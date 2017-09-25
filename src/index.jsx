b7007bfe5001ee4f165ac56524eee5d8275e0853
Elena Donina <elena_donina@epam.com>
2017-09-25 18:17:31 +0200

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { AppContainer } from 'react-hot-loader';
 import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 
 const render = () => {
-    ReactDom.render(
-        <AppContainer>
-            <App />
-        </AppContainer>,
-        document.getElementById('app')
-    )
+    ReactDom.render((
+        <Router>
+            <App>
+                /* https://habrahabr.ru/post/329996/
+                <Switch>
+                    <Route exact path="/" component={Welcome} />
+
+                    <Route path="/posts">
+                        {(args) => (
+                        <Posts {...args}>
+                            <Switch>
+                                <Route exact path="/posts" component={List} />
+                                <Route path="/posts/article/:name" component={Article} />
+                            </Switch>
+                        </Posts>
+                            )}
+                    </Route>
+
+                    <Route path="/about/:tab/:section" component={About} />
+
+                    <Route path="*" component={NotFound} />
+
+                </Switch>*/
+            </App>
+        </Router>
+    ), document.getElementById('app'));
 }
 
 render();
