26f9bdbe4e37bdd01a707e6a86675ba5d71dfccf
Elena Donina <elena_donina@epam.com>
2017-10-19 15:53:39 +0200

    conflicts

--- src/components/video-item/ItemGenre.jsx
+++ src/components/video-item/ItemGenre.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 
-
-<<<<<<< HEAD
-export class ItemGenre extends React.Component {
-
-    constructor(...args) {
-        super(...args);
-    }
-
-    render() {
-        return (
-            <span className="video-item-genre">
-                Oscar-winning Movies
-            </span>
-        )
-    }
-=======
 export const ItemGenre = () => {
     return (
-        <spna className="video-item-genre">
+        <span className="video-item-genre">
             Oscar-winning Movies
-        </spna>
+        </span>
     )
->>>>>>> c49aad84e6004770be1473129f023f4cbaf46625
 }
